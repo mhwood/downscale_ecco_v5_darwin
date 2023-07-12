eb97cc292878fce793d4486a66950afde9b4dd7c
Mike Wood <mhwood@uci.edu>
2023-07-11 23:55:29 -0700

    adding L1 *for_grid files

---
+++ L2/L2_Disko_Bay/code/DIAGNOSTICS_VEC_SIZE.h
@@ -0,0 +1,7 @@
+C------------------------------------------------------------------------------|
+C                           DIAGNOSTICS_VEC_SIZE.h
+C------------------------------------------------------------------------------|
+
+      INTEGER, PARAMETER :: VEC_points = 2000
+      INTEGER, PARAMETER :: nVEC_mask = 4
+      INTEGER, PARAMETER :: nSURF_mask = 1
