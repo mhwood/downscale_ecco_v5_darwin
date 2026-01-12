df8c9eec44aef30b6cd795150c7c12d6439d7a0e
Mike Wood <mhwood@uci.edu>
2026-01-12 09:04:06 -0800

    adding Upernavik files to L2

---
+++ L2/L2_Upernavik/code_control/DIAGNOSTICS_VEC_SIZE.h
@@ -0,0 +1,7 @@
+C------------------------------------------------------------------------------|
+C                           DIAGNOSTICS_VEC_SIZE.h
+C------------------------------------------------------------------------------|
+
+      INTEGER, PARAMETER :: VEC_points = 200
+      INTEGER, PARAMETER :: nVEC_mask = 20
+      INTEGER, PARAMETER :: nSURF_mask = 0
